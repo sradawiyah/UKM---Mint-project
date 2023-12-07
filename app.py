cb92355c3d5f6582b92aa0ad706d203ada32ac31
DevandaYudhaBharagus <devanda0308@gmail.com>
2023-12-08 01:14:06 +0700

    update

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+@app.route("/profil")
+def profil():
+    return render_template('profil.html')
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
